7d3c8a612ed54c6fb586530cbd989aa9865f7e81
6WO7FF24EWV30PS\Administrator <1137007578@qq.com>
2018-10-19 14:50:38 +0800

    222

--- gongsi/src/main/java/com/iotek/controller/UserController.java
+++ gongsi/src/main/java/com/iotek/controller/UserController.java
@@ -2,24 +2,22 @@ package com.iotek.controller;
 
 import com.iotek.model.User;
 import com.iotek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
-
-/**
- * Created by 11929 on 2018/10/18.
- */
 @Controller
 public class UserController {
-    @Resource
+    @Autowired
     private UserService userService;
 
-    @RequestMapping("/login")
-    public String login(@ModelAttribute("user") User user)throws Exception{
-        User user1 = userService.getUser(user);
+    @RequestMapping("login")
+    public String login(Model model, User user) throws  Exception{
+        User user1=userService.getUser(user);
         if (null!=user1){
+            model.addAttribute("user",user1);
             return "main";
         }
         return "../../index";
